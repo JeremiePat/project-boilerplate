a239eadddf7530e51cb4efdca16fc520d69b7fcd
Jeremie Patonnier <jeremie.patonnier@gmail.com>
2014-05-09 16:22:29 +0200

    Update compass config to handle dev/prod build

--- config.dev.rb
+++ config.dev.rb
@@ -3,19 +3,19 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "src/sass"
-css_dir = "build/prod/css"
-images_dir = "build/prod/img"
-javascripts_dir = "build/prod/js"
+css_dir = "build/dev/css"
+images_dir = "build/dev/img"
+javascripts_dir = "build/dev/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
